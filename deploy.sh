098f8ed1b1c20ea8df473797ce30719b32a7004a
devopsbha <devopsbha@gmail.com>
2022-07-29 08:45:47 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+updating in master branch
+updating in development branch
+updating in master branch
 Updating in dev branch
+
 This file contains the deployment script
 Updated manually in GitHub GUI...
+
